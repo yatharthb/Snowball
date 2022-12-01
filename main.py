0b229909723a6ac5ab285b8594a697afa0614eb2
yatharthb <51863781+yatharthb@users.noreply.github.com>
2022-12-01 21:59:57 +0000

    Created the Snowball process

--- main.py
+++ main.py
@@ -20,12 +20,6 @@ sim.run()
 
 # Print the final state of the algorithm
 print(snowball.state)
-In this example, the Snowball algorithm is defined as a Process in simPy. This allows the algorithm to be executed concurrently with other processes in the simulation.
-
-To run the algorithm concurrently, an instance of the Simulation class is created and the Snowball algorithm process is started using the process method. The run method of the Simulation class is then used to run the simulation until the algorithm reaches consensus.
-
-Once the simulation is finished, the final state of the algorithm can be accessed and printed. You can adjust the specific details of the concurrent execution to suit your needs and requirements.
-
 
 
 
