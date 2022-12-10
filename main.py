8be79bcf342d2bfe5cadb91318b9ab6d5b5743ee
yatharthb <51863781+yatharthb@users.noreply.github.com>
2022-12-09 23:22:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ class Snowball(simpy.Process):
         super().__init__(env, generator=self.run())
         self.state = state
         self.nodes = nodes
-        
+        print(state)
+        print(nodes)
     def run(self):
         # Poll 3 random nodes and check if the majority have the same state as this node
         while True:
@@ -26,33 +27,31 @@ class Snowball(simpy.Process):
 # Create an instance of the Environment class
 env = simpy.Environment()
 
-# Create 5 instances of the Snowball algorithm, 2 with state A and 3 with state B
-# TODO: initiate the snowball instances with an array instead of calling the Snowball class
-snowball1 = Snowball(env, "A", [])
-snowball2 = Snowball(env, "A", []) 
-snowball3 = Snowball(env, "B", [])
-snowball4 = Snowball(env, "B", [])
-snowball5 = Snowball(env, "B", [])
-#Add each Snowball algorithm instance to the list of nodes of the other instances
-snowball1.nodes = [snowball2, snowball3, snowball4, snowball5] 
+# Create the state and nodes dictionaries
+state = {"snowball1": "A", "snowball2": "A", "snowball3": "B", "snowball4": "B", "snowball5": "B"}
+nodes = {"snowball1": ["snowball2", "snowball3", "snowball4", "snowball5"],
+         "snowball2": ["snowball1", "snowball3", "snowball4", "snowball5"],
+         "snowball3": ["snowball1", "snowball2", "snowball4", "snowball5"],
+         "snowball4": ["snowball1", "snowball2", "snowball3", "snowball5"],
+         "snowball5": ["snowball1", "snowball2", "snowball3", "snowball4"]}
+# Create instances of the Snowball class
+snowball1 = Snowball(env, state["snowball1"], nodes["snowball1"])
+snowball2 = Snowball(env, state["snowball2"], nodes["snowball2"])
+snowball3 = Snowball(env, state["snowball3"], nodes["snowball3"])
+snowball4 = Snowball(env, state["snowball4"], nodes["snowball4"])
+snowball5 = Snowball(env, state["snowball5"], nodes["snowball5"])
 
-snowball2.nodes = [snowball1, snowball3, snowball4, snowball5] 
-
-snowball3.nodes = [snowball1, snowball2, snowball4, snowball5] 
-
-snowball4.nodes = [snowball1, snowball2, snowball3, snowball5] 
-
-snowball5.nodes = [snowball1, snowball2, snowball3, snowball4] 
-#Start the 5 Snowball algorithm processes concurrently 
+# Start the 5 Snowball algorithm processes concurrently
 env.process(snowball1)
 env.process(snowball2)
 env.process(snowball3)
 env.process(snowball4)
 env.process(snowball5)
 
-#Run the simulation until all the algorithms have the same state
-while len(set([node.state for node in [snowball1, snowball2, snowball3, snowball4, snowball5]])) > 1:
-   env.step()
+# Run the simulation until all the algorithms have the same state
+while len(set([state[node] for node in ["snowball1", "snowball2", "snowball3", "snowball4", "snowball5"]])) > 1:
+    env.step()
+
 
 #Print the final state of the algorithms
 print(snowball1.state)
